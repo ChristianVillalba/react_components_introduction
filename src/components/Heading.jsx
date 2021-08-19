2ed54735b6d1612af21e21b6bd42d252f459d58f
Christian Villalba UK <79871904+ChristianVillalba@users.noreply.github.com>
2021-08-19 14:08:32 +0100

    Update Heading.jsx

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,7 +1,12 @@
+// We import React from the react module     
 import React from "react";
-
+// We create a function that will return an H1 Element. The name of the function must be PascalCased     
 function Heading() {
   return <h1>My Favourite Foods</h1>;
 }
-
+// We export this function so we can import it in our index.js
 export default Heading;
+
+
+
+ 
